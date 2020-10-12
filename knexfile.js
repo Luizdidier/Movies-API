b4078302cd408f289eb5740e693f41b1b6cc901f
Luiz Didier <luizmdidier@gmail.com>
2020-10-12 01:42:05 -0300

    Adjustment for production

--- knexfile.js
+++ knexfile.js
@@ -28,10 +28,11 @@ module.exports = {
   production: {
     client: "pg",
     connection: {
-      host: "localhost",
-      user: "postgres",
-      password: "s3cr3t",
-      database: "imdb",
+      host: "ec2-34-232-212-164.compute-1.amazonaws.com",
+      user: "wkaaxjzxxosvow",
+      password:
+        "e10707b4d779745ec504a0db61f26c22a8d8b2a02f9c68f16128b902b2340450",
+      database: "d637vcp3k3e63v",
       charset: "utf8",
     },
     useNullAsDefault: true,
